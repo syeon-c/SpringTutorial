3932eecd530db659faf6c40c341d07c2a8b014a5
syeon <gkdltjdus@gmail.com>
2023-01-16 00:14:16 +0900

    product repository에 search 추가

--- src/main/java/com/test1/b1/product/repository/ProductRepository.java
+++ src/main/java/com/test1/b1/product/repository/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.test1.b1.product.repository;
 
 import com.test1.b1.product.domain.Product;
+import com.test1.b1.product.repository.search.ProductSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductSearch {
 }
